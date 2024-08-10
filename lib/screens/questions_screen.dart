6f500dc5f18a33c7fd14e6ccc7398e1db6c4ecbd
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-08-10 12:26:44 +0700

    init 2 more screens

---
+++ lib/screens/questions_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreenWidget extends StatefulWidget {
+  const QuestionScreenWidget({super.key});
+
+  @override
+  State<QuestionScreenWidget> createState() => _QuestionScreenWidgetState();
+}
+
+class _QuestionScreenWidgetState extends State<QuestionScreenWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/quiz_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuizScreenWidget extends StatefulWidget {
+  const QuizScreenWidget({super.key});
+
+  @override
+  State<QuizScreenWidget> createState() => _QuizScreenWidgetState();
+}
+
+class _QuizScreenWidgetState extends State<QuizScreenWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
