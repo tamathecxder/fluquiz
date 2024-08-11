9f7205cbf96d2b5a0ae39f35d67aa2bbe48e592f
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-08-11 13:41:57 +0700

    maximize the width of the widget inside column

--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -16,23 +16,30 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.question,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 16,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.question,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
-          })
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map(
+              (answer) {
+                return AnswerButton(answerText: answer, onTap: () {});
+              },
+            )
+          ],
+        ),
       ),
     );
   }
