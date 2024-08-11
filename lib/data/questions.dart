de29cd0f5c513faf1e1582dbb4b795556d2fcc94
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-08-11 12:54:39 +0700

    fill the dummy data from questions model

---
+++ lib/data/questions.dart
@@ -0,0 +1,24 @@
+import 'package:advance_basics_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What is the capital of France?',
+    ['Paris', 'London', 'Berlin', 'Madrid'],
+  ),
+  QuizQuestion(
+    'What is the capital of Germany?',
+    ['Berlin', 'London', 'Paris', 'Madrid'],
+  ),
+  QuizQuestion(
+    'Who is the current President of the Indonesia?',
+    ['Soekarno', 'Soeharto', 'Jokowi', 'Habibie'],
+  ),
+  QuizQuestion(
+    'What is the capital of Indonesia?',
+    ['Jakarta', 'Bandung', 'Surabaya', 'Semarang'],
+  ),
+  QuizQuestion(
+    'Which is the biggest country in the world?',
+    ['Russia', 'Canada', 'China', 'USA'],
+  ),
+];
