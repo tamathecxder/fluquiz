09ced54e2b85f40104722d787c7ff7a80263a1bf
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-08-13 22:26:27 +0700

    init basic layout of results screen

---
+++ lib/screens/results_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const Text(
+              "You answered X out of Y questions correctly",
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text("(List of the correct answers)"),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Restart Quiz'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
