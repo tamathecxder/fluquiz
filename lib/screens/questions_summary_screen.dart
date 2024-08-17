eb75971b08b798eb563f42ce4ceee7ba2b34fb86
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-08-17 12:08:09 +0700

    add questions summary screen

---
+++ lib/screens/questions_summary_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class QuestionsSummaryScreen extends StatelessWidget {
+  const QuestionsSummaryScreen(this.summary, {super.key});
+
+  final List<Map<String, Object>> summary;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: summary.map((data) {
+        String number = data["question_index"].toString();
+        String question = data["question"].toString();
+        String userAnswer = data["user_answer"].toString();
+        String correctAnswer = data["correct_answer"].toString();
+
+        return Row(
+          children: [
+            Text(number),
+            Expanded(
+              child: Column(
+                children: [
+                  Text(question),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(userAnswer),
+                  Text(correctAnswer),
+                ],
+              ),
+            )
+          ],
+        );
+      }).toList(),
+    );
+  }
+}
